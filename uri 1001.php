39ba82847b80c4a920ce0da0c320e7ea0beecebe
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2023-03-20 17:48:10 -0300

    Create uri 1001.php

---
+++ uri 1001.php
@@ -0,0 +1,10 @@
+<?php
+// lê os valores de A e B
+$A = intval(fgets(STDIN));
+$B = intval(fgets(STDIN));
+
+// calcula a soma de A e B
+$soma = $A + $B;
+
+// exibe o resultado
+echo "X = " . $soma . "\n";
